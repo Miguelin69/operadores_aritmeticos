b587e48c799a7dadcefc68721521f4d60d022eb4
Miguelin69 <miguel2234505@correo.uis.edu.cogit config user.email miguel2234505@correo.uis.edu.co>
2023-02-25 08:21:14 -0500

    se agrego programa en python

---
+++ operadores_aritmeticos.py
@@ -0,0 +1,24 @@
+# operadores_aritmeticos
+print(--------operadores aritmeticos------)
+
+# input
+x = int(input("Digite el valor de x"))
+y = int(input("Digite el valor de y"))
+
+# processing 
+
+s = x + y
+r = x - y
+m = x * y
+d = x / y
+mod = x % y
+de = x // y
+p = x ## y
+
+ print("suma" + str(s))
+ print("resta" + str(r))
+ print("multiplicacion" + str(m))
+ print("Division" + str(d))
+ print("Modulo" + str(mod))
+ print("Division entera" + str(de))
+ print("Potencia" + str(p))
